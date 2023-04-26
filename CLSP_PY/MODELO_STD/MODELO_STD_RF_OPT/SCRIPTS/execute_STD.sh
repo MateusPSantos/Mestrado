2b24187804aa9700635bd7adb4738997d7e612fa
Jesus Ossian <jesusossian@gmail.com>
2023-04-26 09:26:23 +0100

    update results rf opt

--- CLSP_PY/MODELO_STD/MODELO_STD_RF_OPT/SCRIPTS/execute_STD.sh
+++ CLSP_PY/MODELO_STD/MODELO_STD_RF_OPT/SCRIPTS/execute_STD.sh
@@ -5,5 +5,5 @@ fo=std
 
 for id in {1..108} #$(seq 1)
 do
-	python3 clsr_math_std.py c52_${id}.txt >> report/out_${fo}_c52_${id}.txt 8 5
+	python3 clsr_math_std.py c52_${id}.txt >> report/out_${fo}_c52_${id}.txt 6 3
 done
